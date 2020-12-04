3fe54bab001b5274709262e53d9c292377cca0b1
ts 21 <shoukitsuda0310@gmail.com>
2020-12-04 09:32:49 +0900

    fin

--- opt-TT.c
+++ opt-TT.c
@@ -5,24 +5,23 @@
 
 /* いろんなプロトタイプ宣言をここに書く*/
 
-void backtrack(int i, int *C);
+void branch(int i, int *C, double sumThroughput, double provisional);
 
 double calThroughput(int i, int j);
-
 
 int main() {
     /* 変数定義をここに書く */
     int C[NSTA];
-    /* この変数に各 STA が最適解で選択している AP の番号を格納 */
+    double sumThroughput;
+    double provisional;
 
+    /* この変数に各 STA が最適解で選択している AP の番号を格納 */
     prepare_input();
     printf("Input prepared\n");
-    for (int i = 0; i < NSTA; i++) {
-    }
-    backtrack(0, C);
 
     /* ここで、最適解を計算する関数を書き、その接続を配列Cに格納するとともに全てのSTAのスループットの和を出力 */
     /*back track*/
+    branch(0, C, 0, 0);
 
     printf("Connections for the throughput: \n");
     for (int i = 0; i < NSTA; i++)
@@ -31,33 +30,45 @@ int main() {
 }
 
 /*以下は関数定義など */
-
-void backtrack(int i, int *C) {
-    if (i == NSTA ) {
+void branch(int i, int *C, double sumThroughput, double provisional) {
+    if (i == NSTA) {
+        printf("Sum of Throughput = %lf \n", sumThroughput);
+        return;
     } else {
-
         provisional = calThroughput(i, 3);
 
         if (calThroughput(i, 0) >= provisional) {
             C[i] = 0;
-            backtrack(i + 1, C);
+            sumThroughput += calThroughput(i, 0);
+            branch(i + 1, C, sumThroughput, provisional);
+
         } else if (calThroughput(i, 1) >= provisional) {
             C[i] = 1;
-            backtrack(i + 1, C);
+            sumThroughput += calThroughput(i, 1);
+            branch(i + 1, C, sumThroughput, provisional);
+
+
         } else if (calThroughput(i, 2) >= provisional) {
             C[i] = 2;
-            backtrack(i + 1, C);
+            sumThroughput += calThroughput(i, 2);
+            branch(i + 1, C, sumThroughput, provisional);
+
         } else if (calThroughput(i, 3) >= provisional) {
             C[i] = 3;
-            backtrack(i + 1, C);
+            sumThroughput += calThroughput(i, 3);
+            branch(i + 1, C, sumThroughput, provisional);
         }
     }
 }
 
 double calThroughput(int i, int j) {
+    double throughput[NSTA][NAP];
     /*Nを計算*/
-    N = sizeof(P[i]);
+
+    N = sizeof(P[i]) / sizeof(double);
     /*throughputを計算*/
     throughput[i][j] = (1 - P[i][j]) / N;
     return throughput[i][j];
 }
+
+
